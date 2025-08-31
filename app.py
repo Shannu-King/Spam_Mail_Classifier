64c89d3f692d7085d2549e39d078ec6a8fabdc8b
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-08-31 08:44:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+app = Flask(__name__)
+
+mail_datas = pd.read_csv('mail_data.csv')
+mails = mail_datas.where((pd.notnull(mail_datas)), '')
+mails.loc[mails['Category'] == 'spam', 'Category'] = 0
+mails.loc[mails['Category'] == 'ham', 'Category'] = 1
+x = mails['Message']
+y = mails['Category']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3)
+feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
+x_train_features = feature_extraction.fit_transform(x_train)
+y_train = y_train.astype('int')
+model = LogisticRegression()
+model.fit(x_train_features, y_train)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        message = request.form["message"]
+        data = feature_extraction.transform([message])
+        prediction = model.predict(data)
+        result = "Spam" if prediction[0] == 0 else "Ham"
+        return render_template("index.html", prediction=result)
+    return render_template("index.html", prediction="")
+
+if __name__ == "__main__":
+    app.run(debug=True)
